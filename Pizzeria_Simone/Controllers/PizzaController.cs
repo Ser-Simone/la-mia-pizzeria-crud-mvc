9bd573c7650346e332683bfef4f6d2ddecf0b83b
Simone Serpi <simoneserpi@hotmail.it>
2022-05-12 16:01:42 +0200

    Aggiungere i file di progetto.

---
+++ Pizzeria_Simone/Controllers/PizzaController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pizzeria_Simone.Models;
+using Pizzeria_Simone.Utils;
+
+namespace Pizzeria_Simone.Controllers
+{
+
+    public class PizzaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Pizza> pizza = PizzaData.GetPizza();
+            return View("HomePage", pizza);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Pizza pizzaFound = GetPizzaById(id);
+
+            if (pizzaFound != null)
+            {
+                return View("Details", pizzaFound);
+            }
+            else
+            {
+                return NotFound("la pizza con id" + id + " non è stato trovato");
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("FormPizza");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza nuovaPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("FormPizza", nuovaPizza);
+            }
+
+            Pizza pizza = new Pizza(PizzaData.GetPizza().Count, nuovaPizza.Title, nuovaPizza.Description, nuovaPizza.Image, nuovaPizza.Price);
+            // il mio modello è corretto
+
+            PizzaData.GetPizza().Add(nuovaPizza);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            Pizza PizzaToEdit = GetPizzaById(id);
+
+            if (PizzaToEdit == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View("Update", PizzaToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, Pizza model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Update", model);
+            }
+            Pizza pizzaOriginal = GetPizzaById(id);
+
+            if (pizzaOriginal != null)
+            {
+                pizzaOriginal.Title = model.Title;
+                pizzaOriginal.Description = model.Description;
+                pizzaOriginal.Image = model.Image;
+
+                return RedirectToAction("Index");
+            } else
+            {
+                return NotFound();
+            }
+        }
+        private Pizza GetPizzaById(int id)
+        {
+            Pizza pizzaFound = null;
+
+            foreach (Pizza pizza in PizzaData.GetPizza())
+            {
+                if (pizza.Id == id)
+                {
+                    pizzaFound = pizza;
+                    break;
+                }
+            }
+            return pizzaFound;
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            int PizzaIndexToRemove = -1;
+
+            List<Pizza> pizzaList = PizzaData.GetPizza();
+
+            for (int i = 0; i < pizzaList.Count; i++)
+            {
+                if (pizzaList[i].Id == id)
+                {
+                    PizzaIndexToRemove = i;
+                }  
+            }
+
+            if (PizzaIndexToRemove >= 0)
+            {
+                PizzaData.GetPizza().RemoveAt(PizzaIndexToRemove);
+
+                return RedirectToAction("Index");
+            }else
+            {
+                return NotFound();
+            }
+        }
+    }
+
+}
+            
---
+++ Pizzeria_Simone/Models/Pizza.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pizzeria_Simone.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Il campo titolo è obbligatorio")]
+        [StringLength(10, ErrorMessage ="Il titolo non può avere più di 10 caratteri")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Il campo descrizione è obbligatorio")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "L'Url è obbligatorio")]
+        [Url(ErrorMessage = "Mi dispiace, l'url non è valido")]
+        public string Image { get; set; }
+        public double Price { get; set; }
+
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza(int id, string Title, string Description, string Image, double Price)
+        {
+            this.Id = Id;
+            this.Title = Title;
+            this.Description = Description; 
+            this.Image = Image;
+            this.Price = Price;
+
+        }
+
+      
+    }
+}
