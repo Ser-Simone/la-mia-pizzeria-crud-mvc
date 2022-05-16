c57f479b6a467e1213c02795c4acdc6783462b59
Simone Serpi <simoneserpi@hotmail.it>
2022-05-16 14:15:41 +0200

    Database Creato

---
+++ Pizzeria_Simone/Data/BlogContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pizzeria_Simone.Data
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=pizza_reustaurante;Integrated Security=True");
+        }
+    }
+}
