24f5dac092e535d4515c4fec3d9502f351580487
Simone Serpi <simoneserpi@hotmail.it>
2022-05-19 14:52:50 +0200

    Aggiunta JS

---
+++ Pizzeria_Simone/Controllers/Api/PizzApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pizzeria_Simone.Data;
+using Pizzeria_Simone.Models;
+
+namespace Pizzeria_Simone.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pizza> listapizza = new List<Pizza>();
+
+            using (BlogContext databPizza = new BlogContext())
+            {
+                listapizza = databPizza.Pizzaset.ToList<Pizza>();
+            }
+            return Ok(listapizza);
+        }
+      
+    }
+}
